32ffc11ed0f20844b841e26d1b54530501a11399
Itamar Tati <itamar.tati1994@gmail.com>
2019-07-03 02:49:04 +0100

    Updates

---
+++ precache-manifest.80129f60f68e796c4016c0e3f0f1d588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4b1836d6afc95a781b31bd7a0a8f94",
+    "url": "/labourCosts/index.html"
+  },
+  {
+    "revision": "46bb90b9afbd61c66a9d",
+    "url": "/labourCosts/static/css/main.7a04c82c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/labourCosts/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "46bb90b9afbd61c66a9d",
+    "url": "/labourCosts/static/js/main.a32ea192.chunk.js"
+  },
+  {
+    "revision": "f1ac997f768bddb36dbb",
+    "url": "/labourCosts/static/js/runtime~main.1fbf6390.js"
+  }
+]);
